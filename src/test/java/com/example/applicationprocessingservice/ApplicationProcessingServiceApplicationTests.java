72955a4eea76b489af135ba4bd6ef766cc86b823
Fotoochkarik <73393992+Fotoochkarik@users.noreply.github.com>
2021-11-30 18:00:21 +0300

    Initial commit

---
+++ src/test/java/com/example/applicationprocessingservice/ApplicationProcessingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.applicationprocessingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationProcessingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
